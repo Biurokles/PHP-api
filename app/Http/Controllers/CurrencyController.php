06cc9e1da2a53b29a878a10127a97072991cc90a
CLEVO Computer <CLEVO@clevo.com>
2023-02-16 14:53:12 +0100

    print całego tego api w controllerze i wyświetlenie na stronie

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
-    //
+    function show()
+    {
+        $url = "http://api.nbp.pl/api/exchangerates/tables/A/";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_response = curl_exec($ch);
+        curl_close($ch);
+        $server_response = json_decode($server_response,true);
+        echo "<pre>"; print_r($server_response); echo"</pre>";
+    }
+
 }
